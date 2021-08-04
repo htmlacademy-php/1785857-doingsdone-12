5e4f3ddca26fabed0d1e34ce21408963e9786a64
ruslan0003 <ruslan0003@yandex.ru>
2021-08-04 22:50:15 +0300

    bugfix: Исправить синтаксические ошибки в sql

--- schema.sql
+++ schema.sql
@@ -1,34 +1,34 @@
-CREATE DATABASE taskcontrol;
-DEFAULT CHARACTER SET utf8;
+CREATE DATABASE taskcontrol
+DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 USE taskcontrol;
 
 CREATE TABLE projects (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT(10) AUTO_INCREMENT PRIMARY KEY,
   project VARCHAR(64),
-  task_id INT,
-  FOREIGN KEY (task_id) REFERENCES tasks (id),
+  task_id INT(10),
+  FOREIGN KEY (task_id) REFERENCES tasks (id)
 );
 
 CREATE TABLE tasks (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT(10) AUTO_INCREMENT PRIMARY KEY,
   date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   status BOOLEAN DEFAULT 0,
   task VARCHAR(255),
   file_url VARCHAR(2083),
-  deadline DATE(),
+  deadline DATE
 );
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT(10) AUTO_INCREMENT PRIMARY KEY,
   date_register DATETIME(),
   email VARCHAR(128) NOT NULL UNIQUE,
   name VARCHAR(64) NOT NULL,
   password CHAR(64),
-  project_id INT,
-  task_id INT,
+  project_id INT(10),
+  task_id INT(10),
   FOREIGN KEY (project_id) REFERENCES projects (id),
-  FOREIGN KEY (task_id) REFERENCES tasks (id),
+  FOREIGN KEY (task_id) REFERENCES tasks (id)
 );
 
 CREATE INDEX deadline ON tasks(deadline);
